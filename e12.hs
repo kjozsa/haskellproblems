e45beff77bfbe3260887dcd43354dd5f87d1e63a
Kristof Jozsa <kristof.jozsa@gmail.com>
2011-09-11 15:14:11 +0200

    cleanup

--- e12.hs
+++ e12.hs
@@ -1,11 +1,11 @@
 factors :: (Integral a) => a -> a
 factors num = foldl (\acc x -> if num `mod` x == 0 then acc+1 else acc) 0 [1..num]
 
-e11 = head $ filter (\z -> factors z > 500) $ map (foldl (+) 0) $ map (\x -> [1..x]) [1..]
+e12 = head $ filter (\z -> factors z > 500) $ map (foldl (+) 0) $ map (\x -> [1..x]) [1..]
 
-e11' = head $ filter (fact500) $ triangles
+e12' = head $ filter (fact500) $ triangles
      where triangles = map (foldl (+) 0) $ map (\x -> [1..x]) [1..]
-           fact500 z = factors z > 500
+           fact500 z = factors z > 5
            factors num = foldl (\acc x -> if num `mod` x == 0 then acc+1 else acc) 0 [1..num]
 
 
